2aaf452ba1ee555150780ef4dfd944e9ca05f80b
Emily Zhao <tong.zhao.92@gmail.com>
2018-03-19 20:07:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,8 @@ class Student
   attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+
+  @@all = []
   def initialize(id=nil, name, grade)
     @name = name
     @grade = grade
